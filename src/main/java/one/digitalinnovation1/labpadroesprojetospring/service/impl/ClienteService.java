7c7c5f5b2426761f338bf45c142501ec9d0224fb
Dev rgvbatera <rgvbatera@outlook.com>
2023-09-17 19:08:21 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation1/labpadroesprojetospring/service/impl/ClienteService.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation1.labpadroesprojetospring.service.impl;
+
+import one.digitalinnovation1.labpadroesprojetospring.model.Cliente;
+
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
---
+++ src/main/java/one/digitalinnovation1/labpadroesprojetospring/service/impl/ViaCepService.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation1.labpadroesprojetospring.service.impl;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import one.digitalinnovation1.labpadroesprojetospring.model.Endereco;
+
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
